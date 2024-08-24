c5135aad50f1017794736b38ce0a249cd31807a8
AliSherifBoudy <120620863+AliSherifBoudy@users.noreply.github.com>
2024-08-24 12:53:26 +0300

    Added generic inheritance to our flag system and quest objectives

---
+++ RPGBots/Assets/Scripts/Flags/GameFlag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class GameFlag<T> : GameFlag
+{
+    public T Value { get; protected set; }
+
+    void OnDisable() => Value = default;
+
+    void OnEnable() => Value = default;
+}
---
+++ RPGBots/Assets/Scripts/Flags/GameFlag_1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+public class GameFlag: ScriptableObject
+{
+    public event Action Changed;
+
+    protected void SendChanged() => Changed?.Invoke();
+}
